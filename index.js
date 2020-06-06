2fb3de5a01b53b3451ca5e9c4b772eb96ccd356b
Sofi Mirosans <sofiamirosans@gmail.com>
2020-06-06 17:50:41 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+$("#js-shopping-list-form").click(function(){
+$("#shopping-list").append('<li><a href="#">New list item</a></li>');
+});
+});
+
+//add item
+//check item
+//delete item 
